1c3b8447dde72cc31c3dc2a18e68a9dfd250a853
Tumeyy <stefan.tumey@gmail.com>
2022-09-11 21:45:15 -0400

    Adding redux store into app.js

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
+import { store } from './store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Let's build UBER!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Let's build UBER!</Text>
+      </View>
+    </Provider>
   );
 }
 
