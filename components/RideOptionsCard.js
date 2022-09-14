cd03758c21e3ff9a561c343f8984b3e57ef58ab4
Tumeyy <stefan.tumey@gmail.com>
2022-09-13 22:17:14 -0400

    Added EatsScreen, began work on RideOptionsCard

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,17 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
+import { Icon } from 'react-native-elements';
 
 const RideOptionsCard = () => {
   return (
-    <SafeAreaView>
-      <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity style={tw`absolute top-3 left-5`}>
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
     </SafeAreaView>
   )
 }
